bc9c5bdae456a5a8e12e139be758ff5c0513cbc7
Neelkanth <61936264+NeelkanthSingh@users.noreply.github.com>
2024-04-29 15:05:25 +0530

    Update SignIn.jsx http->https

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -37,7 +37,7 @@ const SignIn = () => {
   }, [auth, refresh]);
 
   const login = () => {
-    window.location.href = "http://api.versionvaulthub.com/auth/google";
+    window.location.href = "https://api.versionvaulthub.com/auth/google";
   };
 
   return (
